4dea93b327b54691ef9c552eb66de030cb75d9e2
gonghuihui55 <gonghuihui55@163.com>
2018-08-31 16:36:11 +0800

    first commit

---
+++ src/main/java/com/demo/HelloWorld.java
@@ -0,0 +1,1 @@
+package com.demo;/** * @program: AutoTestDemo * @description: HelloWorld * @author: gonghuihui * @create: 2018-08-31 16:30 **/public class HelloWorld {    public static void main(String args[]){        System.out.println("hello world!");    }}
