78cd9ba2794e49c07c7048c82d71cc8d4ba1e9bc
gonghuihui55 <gonghuihui55@163.com>
2018-09-11 10:13:49 +0800

    modified HelloWorld

--- src/main/java/com/demo/HelloWorld.java
+++ src/main/java/com/demo/HelloWorld.java
@@ -1,1 +1,1 @@
-package com.demo;/** * @program: AutoTestDemo * @description: HelloWorld * @author: gonghuihui * @create: 2018-08-31 16:30 **/public class HelloWorld {    public static void main(String args[]){        System.out.println("hello world!");    }}
+package com.demo;/** * @program: AutoTestDemo * @description: HelloWorld * @author: gonghuihui * @create: 2018-08-31 16:30 **/public class HelloWorld {    public static void main(String args[]){        System.out.println("hello world!");        System.out.println("hello Huihui");    }}
